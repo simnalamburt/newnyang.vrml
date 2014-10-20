82647d70dd65e5983ad47ee9280f08aa3b830943
Hyeon Kim <simnalamburt@gmail.com>
2014-10-20 23:43:18 +0900

    getPos()

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function changeColor(e) {
-  console.log(e);
+function getPos(e) {
+  var xyz = e.hitPnt;
+  document.getElementById('result').innerHTML =
+    'X : ' + xyz[0] + '<br>'+
+    'Y : ' + xyz[1] + '<br>'+
+    'Z : ' + xyz[2];
+}
 
+function changeColor(e) {
   var obj = e.hitObject;
   obj.querySelector('#color').diffusecolor =
     '#' + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
